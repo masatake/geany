d30d91867b26b376dd4444e6a9b8128ed8ebeca9
Colomban Wendling <ban@herbesfolles.org>
2010-10-14 16:24:44 +0200

    Add MIME-type filtering in file chooser dialog

--- src/filetypes.c
+++ src/filetypes.c
@@ -1194,6 +1194,8 @@ GtkFileFilter *filetypes_create_file_filter(const GeanyFiletype *ft)
 	{
 		gtk_file_filter_add_pattern(new_filter, ft->pattern[i]);
 	}
+	if (ft->mime_type)
+		gtk_file_filter_add_mime_type(new_filter, ft->mime_type);
 
 	return new_filter;
 }
